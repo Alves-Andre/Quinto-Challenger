c798b9de4130744f5104469d771ebae57933e3a7
André Alves <andrezzinhohh@gmail.com>
2022-02-16 13:25:37 -0300

    new update

--- desafio-gerador-rgb-main/script.js
+++ desafio-gerador-rgb-main/script.js
@@ -1,2 +1,23 @@
-var data = new Date;
-alert(data)
+var data = dataAtualFormatada();
+var hora = horaAtual();
+document.getElementById('datahora').innerText = data+' - '+hora;
+
+function dataAtualFormatada(){
+    var data = new Date,
+        dia = data.getDate().toString(),
+        diaF = (dia.length==1) ? '0'+dia : dia,
+        mes = (data.getMonth()+1).toString(),
+        mesF = (mes.length == 1) ? '0'+mes : mes,
+        anoF = data.getFullYear();
+    return diaF+"/"+mesF+'/'+anoF
+}
+function horaAtual(){
+    var data= new Date,
+        hora = data.getHours().toString(),
+        horaF = (hora.length==1) ? '0'+hora : hora,
+        minutos = data.getMinutes().toString(),
+        minutosF = (minutos.length==1)? '0'+minutos : minutos,
+        segundos = data.getSeconds().toString(),
+        segundosF = (segundos==1)? '0'+segundos : segundos;
+    return hora+':'+minutosF+':'+segundosF
+}
